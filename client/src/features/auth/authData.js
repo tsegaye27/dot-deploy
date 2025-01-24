c1452e88d8795f8be10303869331955a7c729323
tsegaye27 <ttalegn29@gmail.com>
2025-01-24 12:08:16 +0300

    made some changes

--- client/src/features/auth/authData.js
+++ client/src/features/auth/authData.js
@@ -14,7 +14,7 @@ export const storeUserData = (user) => {
 		avatarUrl: userData.avatarUrl,
 	};
 
-	Cookies.set("jwt", jwt, { expires: 1 });
+	Cookies.set("jwt", jwt, { expires: 1000 * 60 * 60 * 24 });
 
 	localStorage.setItem("userData", JSON.stringify(data));
 };
@@ -22,7 +22,7 @@ export const storeUserData = (user) => {
 export const getUserData = (isToken = false) => {
 	const storedData = JSON.parse(localStorage.getItem("userData"));
 	const token = Cookies.get("jwt");
-
+    console.log(token)
 	if (storedData && token && storedData.expiry > Date.now()) {
 		if (isToken) {
 			return token;
